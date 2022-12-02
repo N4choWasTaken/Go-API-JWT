9880c4f90c17147460858a9aa2b2944acf2a43a7
N4choWasTaken <thierrygmuer@outlook.com>
2022-12-02 10:50:45 +0100

    :lock: Router gets created and routes can be secured

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"www.github.com/N4choWasTaken/Go-API-JWT/controllers"
 	db "www.github.com/N4choWasTaken/Go-API-JWT/database"
+	middlewares "www.github.com/N4choWasTaken/Go-API-JWT/middleware"
 )
 
 func main() {
 	// Initialize Database
 	db.Connect("host=localhost user=postgres password=mysecretpassword dbname=postgres port=5455 sslmode=disable")
 	db.Migrate()
+	// Initialize Router
+	router := initRouter()
+	router.Run(":8081")
+}
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.POST("/token", controllers.GenerateToken)
+		api.POST("/user/register", controllers.RegisterUser)
+		secured := api.Group("/secured").Use(middlewares.Auth())
+		{
+			secured.GET("/ping", controllers.Ping)
+		}
+	}
+	return router
 }
